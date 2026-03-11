fd2adb50cd8dfe4edc7fffe099c8843a4b368658
unknown <YY116@ccs.local>
2026-03-11 18:21:58 +0800

    applied to activity 2 - 4 -Gaviola

---
+++ role-based-app-backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+const SECRET_KEY = "your_very_secret_key";
+
+app.use(cors({
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500']
+}));
+
+app.use(express.json());
+
+let users = [
+    {id: 1, username: 'admin', password: bcrypt.hashSync('admin123', 10), role: 'admin'},
+    {id: 2, username: 'alice', password: bcrypt.hashSync('user123', 10), role: 'user'}];
